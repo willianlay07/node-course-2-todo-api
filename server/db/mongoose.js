6e7379cb729de084d81da12d319e4004e724b1cb
willianlay07 <willianlay07@gmail.com>
2017-05-20 01:31:23 +0800

    Update myown mLab Mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose    = require('mongoose');
 
 mongoose.Promise  = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://willianlay07:willianlay07@ds147821.mlab.com:47821/wai-todos' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports  = {mongoose};
