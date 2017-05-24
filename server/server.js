82706db3629777e296673a2b308f97e44cd1d9ca
willianlay07 <willianlay07@gmail.com>
2017-05-25 00:29:10 +0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -104,7 +104,23 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// Model Object & Instance Object;
+// Model Object call with Uppercase User
+// Instance Object call with smallercase user individual user;
 
+// POST / users;
+app.post('/users', (req, res) => {
+    var body    = _.pick(req.body, ['email', 'password']);
+    var user    = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
